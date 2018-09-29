841bf5c7d170de48bf5a4b587e98d818d301b1f2
Steve <steve@itsdcode.com>
2018-09-29 15:54:52 +0900

    - modified function to async

--- aws-sqs.js
+++ aws-sqs.js
@@ -2,7 +2,7 @@ const AWS = require('aws-sdk')
 const sqs = new AWS.SQS()
 const debug = require('debug')('dcode-serverless:lib:aws-sqs')
 
-function receiveMessages (queueUrl, maxNumberOfMessages = 10, region = null) {
+async function receiveMessages (queueUrl, maxNumberOfMessages = 10, region = null) {
   return new Promise((resolve, reject) => {
     if (region) {
       sqs.config.update({'region': region})
@@ -25,23 +25,53 @@ function receiveMessages (queueUrl, maxNumberOfMessages = 10, region = null) {
   })
 }
 
-function deleteMessage (queueUrl, ReceiptHandle, region = null) {
-  if (region) {
-    sqs.config.update({'region': region})
-  }
+async function deleteMessage (queueUrl, ReceiptHandle, region = null) {
+  return Promise((resolve, reject) => {
+    if (region) {
+      sqs.config.update({'region': region})
+    }
 
-  var params = {
-    'QueueUrl': queueUrl,
-    'ReceiptHandle': ReceiptHandle
-  }
+    var params = {
+      'QueueUrl': queueUrl,
+      'ReceiptHandle': ReceiptHandle
+    }
 
-  sqs.deleteMessage(params, function (err, data) {
-    if (err) console.log(err, err.stack) // an error occurred
-    else console.log('DELETED MESSAGE', data)           // successful response
+    sqs.deleteMessage(params, function (err, data) {
+      if (err) {
+        console.error(err, err.stack)
+        return reject(err)
+      }
+
+      console.log('DELETED MESSAGE', data)           // successful response
+      return resolve(data)
+    })
+  })
+}
+
+async function sendMessage (queueUrl, message, region) {
+  return new Promise((resolve, reject) => {
+    if (region) {
+      sqs.config.update({'region': region})
+    }
+
+    var params = {
+      'QueueUrl': queueUrl,
+      'MessageBody': message
+    }
+
+    sqs.sendMessage(params, function (err, data) {
+      if (err) {
+        console.error(err, err.stack)
+        return reject(err)
+      }
+
+      return resolve(data)
+    })
   })
 }
 
 module.exports = {
   'receiveMessages': receiveMessages,
-  'deleteMessage': deleteMessage
+  'deleteMessage': deleteMessage,
+  'sendMessage': sendMessage
 }
